992ba88624ce9cafa7e82f2cdf41c43338159d19
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2025-05-26 14:07:06 +0900

    タイトルにUIねじ込んだ

--- DirectXGame/Game/Scene/TitleScene/TitleScene.cpp
+++ DirectXGame/Game/Scene/TitleScene/TitleScene.cpp
@@ -15,54 +15,37 @@ TitleScene::~TitleScene() {}
 void TitleScene::SetGlobalVariables() {
 	DaiEngine::GlobalVariables* globalVariables = DaiEngine::GlobalVariables::GetInstance();
 
-	//タイトル文字の調整項目追加
-	std::string groupName = "TitleText";
-	globalVariables->CreateGroup(groupName);
-	globalVariables->AddItem(groupName, "Translation", titleText_->worldTransform_.translation_);
-	globalVariables->AddItem(groupName, "Rotation", titleText_->worldTransform_.rotation_);
-	globalVariables->AddItem(groupName, "Scale", titleText_->worldTransform_.scale_);
+	
 	//AボタンUIの調整項目追加
-	groupName = "UI_AButton";
+	std::string groupName = "TitleLogo";
 	globalVariables->CreateGroup(groupName);
-	globalVariables->AddItem(groupName, "Translation", Abutton_->GetPosition());
-	globalVariables->AddItem(groupName, "EffectSpeed", AbuttonEffectParam_.speed_);
-	globalVariables->AddItem(groupName, "StartScale", AbuttonEffectParam_.startScale_);
-	globalVariables->AddItem(groupName, "EndScale", AbuttonEffectParam_.endScale_);
+	globalVariables->AddItem(groupName, "Translation", titleLogo_->GetPosition());
 	//ワープホールの調整項目追加
-	groupName = "WarpHole";
+	groupName = "UI_GameFinish";
 	globalVariables->CreateGroup(groupName);
-	globalVariables->AddItem(groupName, "Position", warpHolePos_);
+	globalVariables->AddItem(groupName, "Translation", gameFinishUI_->GetPosition());
 	//ゲーム開始演出の調整項目追加
-	groupName = "WarpTransition";
+	groupName = "UI_GameStart";
 	globalVariables->CreateGroup(groupName);
-	globalVariables->AddItem(groupName, "Speed", warpTransitionParam_.speed_);
+	globalVariables->AddItem(groupName, "Translation", gameStartUI_->GetPosition());
 
 
 }
 
 void TitleScene::ApplyGlobalVariables() {
 	DaiEngine::GlobalVariables* globalVariables = DaiEngine::GlobalVariables::GetInstance();
-	//タイトル文字のパラメータ設定
-	std::string groupName = "TitleText";
-	titleText_->worldTransform_.translation_ = globalVariables->GetVec3Value(groupName, "Translation");
-	titleText_->worldTransform_.rotation_ = globalVariables->GetVec3Value(groupName, "Rotation");
-	titleText_->worldTransform_.scale_ = globalVariables->GetVec3Value(groupName, "Scale");
+	
 	//AボタンUIのパラメータ設定
-	groupName = "UI_AButton";
-	Abutton_->SetPosition(globalVariables->GetVec2Value(groupName, "Translation"));
-	AbuttonEff_->SetPosition(Abutton_->GetPosition());
-	AbuttonEffectParam_.speed_ = globalVariables->GetFloatValue(groupName, "EffectSpeed");
-	AbuttonEffectParam_.startScale_ = globalVariables->GetFloatValue(groupName, "StartScale");
-	AbuttonEffectParam_.endScale_ = globalVariables->GetFloatValue(groupName, "EndScale");
+	std::string groupName = "TitleLogo";
+	titleLogo_->SetPosition(globalVariables->GetVec2Value(groupName, "Translation"));
+	
 	//ワープホールのパラメータ設定
-	groupName = "WarpHole";
-	warpHolePos_ = globalVariables->GetVec3Value(groupName, "Position");
-	//ゲーム開始演出のカメラ移動の終了地点をワープホールの位置に
-	warpTransitionParam_.endCameraPos_ = warpHolePos_;
+	groupName = "UI_GameFinish";
+	gameFinishUI_->SetPosition(globalVariables->GetVec2Value(groupName, "Translation"));
 
 	//ゲーム開始演出のパラメータ設定
-	groupName = "WarpTransition";
-	warpTransitionParam_.speed_ = globalVariables->GetFloatValue(groupName, "Speed");
+	groupName = "UI_GameStart";
+	gameStartUI_->SetPosition(globalVariables->GetVec2Value(groupName, "Translation"));
 
 
 }
@@ -81,45 +64,28 @@ void TitleScene::Init() {
 	DaiEngine::Object3d::SetSpotLight(&spotLight_);
 
 	///テクスチャ読み込み
-	uint32_t AbuttonTex = DaiEngine::TextureManager::Load("AButton.png");
-	uint32_t backGroundTex = DaiEngine::TextureManager::Load("Black.png");
-	uint32_t AbuttonEffectTex = DaiEngine::TextureManager::Load("ButtonEffect.png");
-	uint32_t fadeSpriteTex = DaiEngine::TextureManager::Load("white.png");
+	uint32_t titleLogoTex = DaiEngine::TextureManager::Load("titleLogo.png");
+	uint32_t gameStartTex = DaiEngine::TextureManager::Load("GameStart.png");
+	uint32_t gameFinifhTex = DaiEngine::TextureManager::Load("GameFinish.png");
 	///
 
-	//モデル読み込み
-	std::shared_ptr<DaiEngine::Model> titleTextModel = DaiEngine::ModelManager::LoadOBJ("TitleText");
+	titleLogo_.reset(DaiEngine::Sprite::Create(titleLogoTex, {}));
 
-	//タイトル文字初期化
-	titleText_.reset(DaiEngine::Object3d::Create(titleTextModel));
+	gameStartUI_.reset(DaiEngine::Sprite::Create(gameStartTex, {}));
+	gameStartUI_->SetSize({ 350.0f,70 });
+	gameStartUI_->SetTextureArea({ 350.0f * static_cast<float>(gStartUISwitch_),0.0f }, { 350.0f,70 });
+
+	gameFinishUI_.reset(DaiEngine::Sprite::Create(gameFinifhTex, {}));
+	gameFinishUI_->SetSize({ 350.0f,70 });
+	gameFinishUI_->SetTextureArea({ 350.0f * static_cast<float>(gFinishUISwitch_),0.0f }, { 350.0f,70 });
 	
-	//背景初期化
-	backGround_.reset(DaiEngine::Sprite::Create(backGroundTex, { DaiEngine::WinApp::kClientWidth / 2.0f,DaiEngine::WinApp::kClientHeight / 2.0f }));
-	backGround_->SetSize(Vector2(DaiEngine::WinApp::kClientWidth, DaiEngine::WinApp::kClientHeight));
 
-	//フェード用スプライト初期化
-	fadeSprite_.reset(DaiEngine::Sprite::Create(fadeSpriteTex, { DaiEngine::WinApp::kClientWidth / 2.0f,DaiEngine::WinApp::kClientHeight / 2.0f }));
-	fadeSprite_->SetSize(Vector2(DaiEngine::WinApp::kClientWidth, DaiEngine::WinApp::kClientHeight));
-
-	//AボタンUI初期化
-	Abutton_.reset(DaiEngine::Sprite::Create(AbuttonTex, {}));
-	AbuttonEff_.reset(DaiEngine::Sprite::Create(AbuttonEffectTex, {}));
-
-	///エフェクト読み込み
-	//道
-	runWay_ = ParticleManager::Load("RunWay");
-	//ワープホール
-	warpHole_ = ParticleManager::Load("WarpHole");
-
-	//ゲーム開始演出初期化
-	WarpTransitionInit();
 
 	//調整項目の追加と代入
 	SetGlobalVariables();
 	ApplyGlobalVariables();
 	
-	//行列更新
-	titleText_->worldTransform_.UpdateMatrix();
+	
 }
 
 void TitleScene::Update() {
@@ -135,39 +101,35 @@ void TitleScene::Update() {
 	if (DaiEngine::Input::GetInstance()->PushKey(DIK_LCONTROL) && DaiEngine::Input::GetInstance()->TriggerKey(DIK_3)) {
 		DaiEngine::SceneManager::GetInstance()->ChangeScene("Debug");
 	}
-	//デバッグ用ゲーム開始演出開始
-	if (DaiEngine::Input::GetInstance()->TriggerKey(DIK_H)) {
-		warpTransitionParam_.isTransition_ = true;
-	}
+	
 
 #endif // _DEBUG
-	//Aボタンでゲーム開始
-	if (DaiEngine::Input::GetInstance()->TriggerButton(DaiEngine::Input::Button::A)) {
-		warpTransitionParam_.isTransition_ = true;
+	
+
+	switch (select_) {
+		case Select::Start:
+
+			if (DaiEngine::Input::GetInstance()->TriggerKey(DIK_DOWN)) {
+				select_ = Select::Finish;
+				gStartUISwitch_ = UISwitch::Off;
+				gFinishUISwitch_ = UISwitch::On;
+
+			}
+
+			break;
+		case Select::Finish:
+
+			if (DaiEngine::Input::GetInstance()->TriggerKey(DIK_UP)) {
+				select_ = Select::Start;
+				gStartUISwitch_ = UISwitch::On;
+				gFinishUISwitch_ = UISwitch::Off;
+			}
+
+			break;
 	}
 
-	if (warpTransitionParam_.isTransition_) {
-		//ゲーム開始演出
-		WarpTransitionUpdate();
-	}
-
-	//UIエフェクト更新
-	UIEffectUpdate();
-
-	///エフェクト更新
-	//道
-	for (auto& [group, particle] : runWay_) {
-		particle->particleData_.emitter_.translate = runWayPos_;
-		particle->Update();
-	}
-	//ワープホール
-	for (auto& [group, particle] : warpHole_) {
-		particle->particleData_.emitter_.translate = warpHolePos_;
-		particle->Update();
-	}
-
-	//行列更新
-	titleText_->worldTransform_.UpdateMatrix();
+	gameStartUI_->SetTextureArea({ 350.0f * static_cast<float>(gStartUISwitch_),0.0f }, { 350.0f,70 });
+	gameFinishUI_->SetTextureArea({ 350.0f * static_cast<float>(gFinishUISwitch_),0.0f }, { 350.0f,70 });
 	
 	//ライト更新
 	pointLight_.Update();
@@ -179,13 +141,13 @@ void TitleScene::Update() {
 
 void TitleScene::DrawBackGround(){
 
-	backGround_->Draw();
+	
 
 }
 
 void TitleScene::DrawModel(){
 
-	titleText_->Draw(camera_);
+	
 
 }
 
@@ -197,23 +159,15 @@ void TitleScene::DrawParticleModel(){
 
 void TitleScene::DrawParticle(){
 
-	for (auto& [group, particle] : runWay_) {
-		particle->Draw(camera_);
-	}
-	for (auto& [group, particle] : warpHole_) {
-		particle->Draw(camera_);
-	}
+	
 
 }
 
 void TitleScene::DrawUI(){
 
-	if (!warpTransitionParam_.isTransition_) {
-		AbuttonEff_->Draw();
-		Abutton_->Draw();
-	}else {
-		fadeSprite_->Draw();
-	}
+	titleLogo_->Draw();
+	gameStartUI_->Draw();
+	gameFinishUI_->Draw();
 	
 }
 
@@ -237,58 +191,3 @@ void TitleScene::DebugGUI(){
 
 #endif // _DEBUG
 }
-
-void TitleScene::UIEffectInit() {
-
-	AbuttonEffectParam_.cycle_ = 0.0f;
-	AbuttonEffectParam_.alpha_ = 1.0f;
-
-}
-
-void TitleScene::UIEffectUpdate() {
-
-	AbuttonEffectParam_.cycle_ += AbuttonEffectParam_.speed_;
-	//1.0fを超えたら0.0fから
-	AbuttonEffectParam_.cycle_ = std::fmod(AbuttonEffectParam_.cycle_, 1.0f);
-
-	//徐々に透明に
-	AbuttonEffectParam_.alpha_ = Lerp(AbuttonEffectParam_.cycle_, 1.0f, 0.0f);
-	float scale{};
-	//少しずつ大きく
-	scale = Lerp(AbuttonEffectParam_.cycle_, AbuttonEffectParam_.startScale_, AbuttonEffectParam_.endScale_);
-
-	AbuttonEff_->SetColor(Vector4(1.0f, 1.0f, 1.0f, AbuttonEffectParam_.alpha_));
-	AbuttonEff_->SetScale(scale);
-
-}
-
-void TitleScene::WarpTransitionInit() {
-
-	warpTransitionParam_.param_ = 0.0f;
-	warpTransitionParam_.alpha_ = 0.0f;
-	warpTransitionParam_.startCameraPos_ = camera_.translation_;
-
-}
-
-void TitleScene::WarpTransitionUpdate() {
-
-	warpTransitionParam_.param_ += warpTransitionParam_.speed_;
-	warpTransitionParam_.param_ = std::clamp(warpTransitionParam_.param_, 0.0f, 1.0f);
-
-	float T = Easing::easeInCirc(warpTransitionParam_.param_);
-	//カメラの移動
-	camera_.translation_ = Lerp(T, warpTransitionParam_.startCameraPos_, warpTransitionParam_.endCameraPos_);
-
-	T = Easing::easeInExpo(warpTransitionParam_.param_);
-	//α値の変化
-	warpTransitionParam_.alpha_ = Lerp(T, 0.0f, 1.0f);
-
-	fadeSprite_->SetColor(Vector4(1.0f, 1.0f, 1.0f, warpTransitionParam_.alpha_));
-
-	//演出が終わったらシーンチェンジ
-	if (warpTransitionParam_.param_ >= 1.0f) {
-		DaiEngine::SceneManager::GetInstance()->ChangeScene("Game");
-	}
-
-}
-
