9d73ae8a7e68027865a7df32d475e01b9566e3db
Taka <k022g0068@g.neec.ac.jp>
2025-05-30 14:20:54 +0900

    仮でコライダー追加

--- DirectXGame/Game/Player/Player.cpp
+++ DirectXGame/Game/Player/Player.cpp
@@ -24,9 +24,8 @@ Player::Player()
 	//コライダークラス生成
 	collider_ = std::make_unique<DaiEngine::SphereCollider>();
 	collider_->Init("player",*world_,radius_);
-	collider_->ColliderOn();
 	DaiEngine::ColliderManager::GetInstance()->AddCollider(collider_.get());
-	collider_->SetEnterCallback([this](DaiEngine::Collider*) { OnCollison(); });
+
 	//プレイヤーポインタ設定
 	IProtBehavior::SetPlayer(this);
 
