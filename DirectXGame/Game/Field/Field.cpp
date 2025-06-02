d29f8368737564a57a43e40f30b21459b7765303
Y04Kato <k022g0046@g.neec.ac.jp>
2025-06-02 16:05:27 +0900

    Fixed bugs in field generation

--- DirectXGame/Game/Field/Field.cpp
+++ DirectXGame/Game/Field/Field.cpp
@@ -18,15 +18,9 @@ void Field::Initialize() {
 
 	//Instancingゲームオブジェクト
 	instancingObj_ = std::make_unique<InstancingGameObject>();
-	instancingObj_->Init("Cube", 1000);
+	instancingObj_->Init("Cube", 10000);
 
 	worldTransform_.Init();
-
-	//ステージを生成
-	CreateStage();
-
-	//ステージ開始演出
-	StartStage();
 
 	//
 	std::unique_ptr<GVariGroup>gvg = std::make_unique<GVariGroup>("Field");
